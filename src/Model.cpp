e907086a43bbf8c998cb0d48a77448237a5901ad
CLEVER1337 <sanya.veselkov.811@gmail.com>
2022-12-13 18:33:30 +0600

    class Model refactoring

--- src/Model.cpp
+++ src/Model.cpp
@@ -7,17 +7,20 @@ Model::Model()
 }
 
 Model::Model(QOpenGLFunctions_3_3_Core &gl, char* path)
+/*Load model*/
 {
     LoadModel(gl, path);
 }
 
 void Model::Draw(QOpenGLFunctions_3_3_Core &gl, nsUtility::ShaderProgram shaderProgram)
+/*Draw all meshes*/
 {
     for(unsigned int i = 0; i < meshes.size(); i++)
         meshes[i].Draw(gl, shaderProgram);
 }
 
 void Model::LoadModel(QOpenGLFunctions_3_3_Core &gl, std::string path)
+/*Load model*/
 {
     Assimp::Importer importer;
 
@@ -25,47 +28,55 @@ void Model::LoadModel(QOpenGLFunctions_3_3_Core &gl, std::string path)
 
     directory = path.substr(0, path.find_last_of('/'));
 
-    ProccesNode(gl, scene->mRootNode, scene);
+    AdaptateNode(gl, scene->mRootNode, scene);
 }
 
-void Model::ProccesNode(QOpenGLFunctions_3_3_Core &gl, aiNode* node, const aiScene* scene)
+void Model::AdaptateNode(QOpenGLFunctions_3_3_Core &gl, aiNode* node, const aiScene* scene)
+/*Adaptate all meshes in node*/
 {
+    /*Adaptate all polygon meshes in node*/
     for(unsigned int i = 0; i < node->mNumMeshes; i++)
     {
         aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
-        meshes.push_back(ProccesMesh(gl, mesh, scene));
+        meshes.push_back(AdaptateMesh(gl, mesh, scene));
     }
 
+    /*Do adaptate for every node's children*/
     for(unsigned int i = 0; i < node->mNumChildren; i++)
     {
-        ProccesNode(gl, node->mChildren[i], scene);
+        AdaptateNode(gl, node->mChildren[i], scene);
     }
 }
 
-Mesh Model::ProccesMesh(QOpenGLFunctions_3_3_Core &gl, aiMesh* mesh, const aiScene* scene)
+Mesh Model::AdaptateMesh(QOpenGLFunctions_3_3_Core &gl, aiMesh* mesh, const aiScene* scene)
+/*Adaptate mesh*/
 {
     std::vector<Vertex> vertices;
     std::vector<unsigned int> indices;
     std::vector<TextureData> textures;
 
+    /*Adaptate vertexes*/
     for(unsigned int i = 0; i < mesh->mNumVertices; i++)
     {
         Vertex vertex;
 
         glm::vec3 vector;
 
+        /*Vertex*/
         vector.x = mesh->mVertices[i].x;
         vector.y = mesh->mVertices[i].y;
         vector.z = mesh->mVertices[i].z;
 
         vertex.position = vector;
 
+        /*Normal*/
         vector.x = mesh->mNormals[i].x;
         vector.y = mesh->mNormals[i].y;
         vector.z = mesh->mNormals[i].z;
 
         vertex.normal = vector;
 
+        /*Texture coords*/
         if(mesh->mTextureCoords[0])
         {
             glm::vec2 vector2;
@@ -83,7 +94,7 @@ Mesh Model::ProccesMesh(QOpenGLFunctions_3_3_Core &gl, aiMesh* mesh, const aiSce
         vertices.push_back(vertex);
     }
 
-    //indices
+    /*Adaptate indices*/
     for(unsigned int i = 0; i < mesh->mNumFaces; i++)
     {
         aiFace face = mesh->mFaces[i];
@@ -94,15 +105,17 @@ Mesh Model::ProccesMesh(QOpenGLFunctions_3_3_Core &gl, aiMesh* mesh, const aiSce
         }
     }
 
-    //materials
+    /*Adaptate materials*/
     if(mesh->mMaterialIndex >= 0)
     {
         aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
 
+        /*Diffuse map*/
         std::vector<TextureData> diffuseMaps = LoadMaterialTexture(gl, material, aiTextureType_DIFFUSE, DIFFUSE);
 
         textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
 
+        /*Specular map*/
         std::vector<TextureData> specularMaps = LoadMaterialTexture(gl, material, aiTextureType_SPECULAR, SPECULAR);
 
         textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
@@ -112,6 +125,7 @@ Mesh Model::ProccesMesh(QOpenGLFunctions_3_3_Core &gl, aiMesh* mesh, const aiSce
 }
 
 std::vector<TextureData> Model::LoadMaterialTexture(QOpenGLFunctions_3_3_Core &gl, aiMaterial* material, aiTextureType type, TextureType typeName)
+/*Load material*/
 {
     std::vector<TextureData> textures;
 
@@ -119,19 +133,20 @@ std::vector<TextureData> Model::LoadMaterialTexture(QOpenGLFunctions_3_3_Core &g
     {
         aiString str;
         material->GetTexture(type, i, &str);
-        bool skip = false;
+        bool isLoaded = false;
 
         for(unsigned int j = 0; j < texturesLoaded.size(); j++)
         {
             if(std::strcmp(texturesLoaded[j].path.C_Str(), str.C_Str()))
             {
                 textures.push_back(texturesLoaded[j]);
-                skip = true;
+                isLoaded = true;
                 break;
             }
         }
 
-        if(!skip)
+        /*If texture wasn't loaded load it*/
+        if(!isLoaded)
         {
             TextureData texture;
 
@@ -140,6 +155,7 @@ std::vector<TextureData> Model::LoadMaterialTexture(QOpenGLFunctions_3_3_Core &g
             texture.id = textureLoader.GetTextureId();
             texture.type = typeName;
             texture.path = str;
+
             textures.push_back(texture);
             texturesLoaded.push_back(texture);
         }

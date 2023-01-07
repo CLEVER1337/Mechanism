949455f3d5f76c80acfff6556e5c4019dd29ee20
CLEVER1337 <sanya.veselkov.811@gmail.com>
2023-01-07 16:30:38 +0600

    path trace function hemisphere reflections added

--- resource/SHADERS/path_tracer.glsl
+++ resource/SHADERS/path_tracer.glsl
@@ -1,15 +1,17 @@
 #version 330 core
 
-#include "ray.glsl"
-#include "rigid_body.glsl"
-#include "reflection.glsl"
-
 #define FAR_DISTANCE 1000000.0
 #define MAX_REFLECTIONS 8
 
 uniform int u_object_count;
 
+in vec3 frag_pos;
+
 s_rigid_body objects[u_object_count];
+
+#include "ray.glsl"
+#include "rigid_body.glsl"
+#include "reflection.glsl"
 
 bool ray_cast(s_ray ray, out float distance_to_object, out vec3 normal, out s_material material){
     float min_distance = FAR_DISTANCE;
@@ -40,9 +42,23 @@ vec3 path_trace(s_ray ray){
 
         if(ray_cast(ray, distance_to_object, normal, material)){
             new_ray.origin = ray.origin + distance_to_object * ray.direction;
-            //new_ray.direction = ;
+            
+            vec3 hemisphere_distributed_direction = get_random_hemisphere_ray_relative_normal(random2(), normal);
 
-            ray,origin = new_ray.origin;
+            random_vec = normalize(2.0 * random3() - 1.0);
+
+            vec3 tangent = cross(random_vec, normal);
+            vec3 bitangent = cross(normal, tangent);
+            mat3 transform = mat3(tangent, bitangent, normal);
+
+            new_ray.direction = transform * hemisphere_distributed_direction;
+
+            vec3 ideal_reflection = reflect(ray.direction, normal);
+            new_ray.direction = normalize(mix(new_ray.direction, ideal_reflection, material.roughness));
+
+            new_ray.origin += normal * 0.8;
+
+            ray.origin = new_ray.origin;
             ray.direction = new_ray.direction;
 
             total_light += reflection_coef * material.emmitance;
--- resource/SHADERS/random.glsl
+++ resource/SHADERS/random.glsl
@@ -1,8 +1,20 @@
 #ifndef RANDOM_GLSL
 #define RANDOM_GLSL
 
-float random(vec2 coef){
+float random_num(vec2 coef){
     return fract(sin(dot(coef.xy, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+float random(){
+    return random_num(frag_pos.xy);
+}
+
+vec2 random2(){
+    return vec2(random(), random());
+}
+
+vec3 random3(){
+    return vec3(random(), random(), random());
 }
 
 #endif
